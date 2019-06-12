806ce5fffbd59bbb09af7fcd2a5ee43f6ae3f303
benjchand <benchand@gmail.com>
2019-06-12 02:51:44 -0700

    Added Nav Bar formatting to new master Team-Member Dashboard

--- src/components/Pages/TeamMemberDashboard/SimpleTabs.js
+++ src/components/Pages/TeamMemberDashboard/SimpleTabs.js
@@ -1,40 +1,57 @@
-import React from 'react'
+import React from "react";
 
-import AppBar from '@material-ui/core/AppBar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
-import TrainingSeries from './TrainingSeries'
-import AllTrainings from './AllTrainings'
-import Responses from './Responses'
+import TrainingSeries from "./TrainingSeries";
+import AllTrainings from "./AllTrainings";
+import Responses from "./Responses";
+
+// import { withStyles } from "@material-ui/core/styles";
+
+import styled from "styled-components";
 
 class SimpleTabs extends React.Component {
   state = {
     value: 0
-  }
+  };
 
   changeTab = (event, newValue) => {
     this.setState({
       value: newValue
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <>
         <AppBar position="static">
-          <Tabs value={this.state.value} onChange={this.changeTab}>
+          <TabsStyled
+            indicatorColor="primary"
+            textColor="primary"
+            value={this.state.value}
+            onChange={this.changeTab}
+          >
             <Tab label="Training Series" />
             <Tab label="All Trainings" />
             <Tab label="Responses" />
-          </Tabs>
+          </TabsStyled>
         </AppBar>
         {this.state.value === 0 && <TrainingSeries />}
         {this.state.value === 1 && <AllTrainings />}
         {this.state.value === 2 && <Responses />}
       </>
-    )
+    );
   }
 }
 
-export default SimpleTabs
+const TabsStyled = styled(Tabs)`
+  background: white;
+  color: black;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+export default SimpleTabs;

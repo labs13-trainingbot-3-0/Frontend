4a794f365359eb302d5eecaa77fcf322f0e964eb
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-06-17 12:12:36 -0400

    Instantiate lock with config

--- src/Auth/AuthPasswordless.js
+++ src/Auth/AuthPasswordless.js
@@ -1,8 +1,19 @@
 import { Auth0LockPasswordless } from 'auth0-lock';
 import { AUTH_CONFIG } from "./auth0-variables";
 import history from "history.js";
+import logo from "../img/training-bot.png"
 
-export const lock = new Auth0LockPasswordless(AUTH_CONFIG.clientId, AUTH_CONFIG.domain, {
+var options = {
+  theme: {
+    logo: logo,
+    primaryColor: '#451476',
+  },
+  languageDictionary: {
+    title: "Training Bot"
+  }
+};
+
+export const lock = new Auth0LockPasswordless(AUTH_CONFIG.clientId, AUTH_CONFIG.domain, options, {
   passwordlessMethod: "link", // Sets Lock to use magic link
   auth: {
     redirectUrl: process.env.REACT_APP_HOST + "team-member",

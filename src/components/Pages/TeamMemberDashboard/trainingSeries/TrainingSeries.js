4a52eb98fd6e8f07aa06bf053895a2a042c01ad0
Joe <yogurmystery@gmail.com>
2019-06-18 23:53:18 -0500

    pulling in training series messages from backend

--- src/components/Pages/TeamMemberDashboard/trainingSeries/TrainingSeries.js
+++ src/components/Pages/TeamMemberDashboard/trainingSeries/TrainingSeries.js
@@ -8,39 +8,29 @@ import ProgressCircle from 'components/UI/Progress/ProgressCircle';
 import './trainingSeries.css'
 import axios from 'axios'
 
-
-// const dummyData = [
-//   { title:"Hazardous Material Policy", image: 'https://truckerpath.com/uploads/2017/02/HAZMAT.jpg', id: 1},
-//   { title:"Taxes and Witholding", image: 'https://www.gannett-cdn.com/-mm-/73d0c51d7092d111f30c6c876cceb8310bcb4ec2/c=0-34-580-360&r=x803&c=1600x800/local/-/media/2017/11/02/USATODAY/usatsports/tax-1040-with-money_large.jpg', id: 2},
-//   { title: "Vacation and Time Off", image: 'http://www.30aluxuryvacations.com/sites/default/files/styles/homepage_slideshow_big/public/slides/slideshow-chairs.jpg?itok=6sZeBUHM', id: 3},
-//   { title: "Company Computer Policy", image: 'https://images.pond5.com/young-businesswoman-working-computer-office-footage-018091108_prevstill.jpeg', id: 4},
-//   { title: "Your Fidelity 401k", image: 'https://fidelitylogin.org/wp-content/uploads/2018/01/Fidelity-Investments-Login.jpg', id: 5}
-// ];
-
 class TrainingSeries extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       series: [],
       seriesModal: false,
-      seriesClicked: 0
+      seriesClicked: -1
     }
-    // this.openSeriesModal = this.openSeriesModal.bind(this)
-    // this.closeSeriesModal = this.closeSeriesModal.bind(this)
   }
 
-  componentDidMount=()=> {
-    axios.get(`${process.env.REACT_APP_API}/api/training-series`)
-    .then(res =>
-      this.setState({
-        ...this.state,
-        ...res.data
-      })
-      )
+  async componentDidMount() {
+    const mySeries = await axios.get(`${process.env.REACT_APP_API}/api/training-series`);
+    console.log(`Check out this mySeries object: `, mySeries.data)
+    const myMessages = await axios.get(`${process.env.REACT_APP_API}/api/messages`)
+    console.log(`Check out this myMessages object: `, myMessages.data)
+    this.setState({
+      trainingSeries: mySeries.data.trainingSeries,
+      messages: myMessages.data.messages
+    })
   }
 
   openSeriesModal = (id) => {
-    console.log(`state = ${this.state}`)
+    console.log(`state = `, this.state)
     this.setState({
       ...this.state,
       seriesModal: true,
@@ -48,7 +38,7 @@ class TrainingSeries extends React.Component {
   })
 }
 
-  closeSeriesModal = (id) => {
+  closeSeriesModal = () => {
     console.log('Hello from closeSeriesModal')
     this.setState({
       ...this.state,
@@ -65,6 +55,7 @@ class TrainingSeries extends React.Component {
           seriesData={this.state.trainingSeries}/>:
         <ProgressCircle />}
         <SeriesModal 
+          messages={this.state.messages}
           seriesId={this.state.seriesClicked}
           show={this.state.seriesModal}
           handleClose={this.closeSeriesModal}

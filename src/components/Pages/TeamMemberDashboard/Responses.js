782490992f76734d1aafd29185ef00e150b1cd74
Divya Nair <divnairs@gmail.com>
2019-06-19 11:36:59 -0700

    wrap Responses with a paper

--- src/components/Pages/TeamMemberDashboard/Responses.js
+++ src/components/Pages/TeamMemberDashboard/Responses.js
@@ -3,6 +3,18 @@ import { connect } from 'react-redux'
 import moment from 'moment'
 import orderBy from 'lodash/orderBy'
 
+import { withStyles } from '@material-ui/styles'
+import Paper from '@material-ui/core/Paper'
+
+const styles = {
+  paper: {
+    margin: '5px auto',
+    padding: '16px',
+    height: '100%',
+    width: '95%'
+  }
+}
+
 class Responses extends React.Component {
   render() {
     const notif = this.props.notifications.map(item => ({
@@ -20,13 +32,13 @@ class Responses extends React.Component {
     const messages = orderBy([...notif, ...resp], ['date'])
 
     return (
-      <>
+      <Paper elevation={2} className={this.props.classes.paper}>
         {messages.map(item => (
           <p>
             {item.name} | {item.text} | {item.date}
           </p>
         ))}
-      </>
+      </Paper>
     )
   }
 }
@@ -39,4 +51,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   null
-)(Responses)
+)(withStyles(styles)(Responses))

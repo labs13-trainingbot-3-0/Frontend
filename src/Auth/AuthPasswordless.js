cbe1bc0eb10e1555f574c757fd89e4449d02e12a
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-06-24 05:22:51 -0400

    Styled lock

--- src/Auth/AuthPasswordless.js
+++ src/Auth/AuthPasswordless.js
@@ -1,14 +1,24 @@
 import { Auth0LockPasswordless } from 'auth0-lock';
 import { AUTH_CONFIG } from "./auth0-variables";
 import history from "history.js";
+import logo from "../img/training-bot.png";
 
-export const lock = new Auth0LockPasswordless(AUTH_CONFIG.clientId, AUTH_CONFIG.domain, {
+var options = {
   passwordlessMethod: "link", // Sets Lock to use magic link
   auth: {
     redirectUrl: process.env.REACT_APP_HOST + "team-member",
     responseType: 'token id_token'
-  }
-});
+  },
+  theme: {
+    logo: logo,
+    primaryColor: '#451476',
+  },
+  languageDictionary: {
+    title: "Training Bot"
+  },
+};
+
+export const lock = new Auth0LockPasswordless(AUTH_CONFIG.clientId, AUTH_CONFIG.domain, options);
 
 export const logout = () => {
   localStorage.clear()

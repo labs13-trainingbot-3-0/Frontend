b89d0167a6b544fabf3c78b9075f5bf6d05ca62d
Divya Nair <divnairs@gmail.com>
2019-06-25 13:16:43 -0700

    skeleton of /team-member appbar

---
+++ src/components/Pages/TeamMemberDashboard/TeamMemberAppBar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { logout } from '../../../Auth/AuthPasswordless'
+import Logo from 'img/training-bot.png'
+
+// MUI
+import { withStyles } from '@material-ui/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import Avatar from '@material-ui/core/Avatar'
+import HelpOutline from '@material-ui/icons/HelpOutline'
+
+const styles = {
+  logo: {
+    height: '35px'
+  }
+}
+
+class TeamMemberAppBar extends React.Component {
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="Home">
+            <img
+              src={Logo}
+              alt="A cute, personable robot"
+              className={this.props.classes.logo}
+            />
+          </IconButton>
+          <IconButton edge="start" color="inherit" aria-label="Help">
+            <HelpOutline />
+          </IconButton>
+          <Avatar
+            src={JSON.parse(localStorage.getItem('Profile')).picture}
+            alt={JSON.parse(localStorage.getItem('Profile')).name}
+          />
+          <Button onClick={() => logout()} color="inherit">
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
+
+export default withStyles(styles)(TeamMemberAppBar)

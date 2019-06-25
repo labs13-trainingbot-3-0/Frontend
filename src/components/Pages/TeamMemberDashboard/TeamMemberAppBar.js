d551037fa436b98fad999a3303e1c39ac1cca48f
Divya Nair <divnairs@gmail.com>
2019-06-25 14:30:49 -0700

    responsively place grid items

--- src/components/Pages/TeamMemberDashboard/TeamMemberAppBar.js
+++ src/components/Pages/TeamMemberDashboard/TeamMemberAppBar.js
@@ -11,6 +11,7 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import Avatar from '@material-ui/core/Avatar'
 import HelpOutline from '@material-ui/icons/HelpOutline'
+import Grid from '@material-ui/core/Grid'
 
 const styles = {
   logo: {
@@ -23,23 +24,34 @@ class TeamMemberAppBar extends React.Component {
     return (
       <AppBar position="static" color="default">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="Home">
-            <img
-              src={Logo}
-              alt="A cute, personable robot"
-              className={this.props.classes.logo}
-            />
-          </IconButton>
-          <IconButton edge="start" color="inherit" aria-label="Help">
-            <HelpOutline />
-          </IconButton>
-          <Avatar
-            src={JSON.parse(localStorage.getItem('Profile')).picture}
-            alt={JSON.parse(localStorage.getItem('Profile')).name}
-          />
-          <Button onClick={() => logout()} color="inherit">
-            Logout
-          </Button>
+          <Grid container justify="flex-start" alignContent="center">
+            <Grid item xs={12} sm={6} md={8} lg={9}>
+              <IconButton aria-label="Home">
+                <img
+                  src={Logo}
+                  alt="A cute, personable robot"
+                  className={this.props.classes.logo}
+                />
+              </IconButton>
+              <IconButton color="inherit" aria-label="Help">
+                <HelpOutline />
+              </IconButton>
+            </Grid>
+          </Grid>
+
+          <Grid container justify="flex-end" alignContent="center">
+            <Grid item xs={4} sm={3} md={2} lg={1}>
+              <Avatar
+                src={JSON.parse(localStorage.getItem('Profile')).picture}
+                alt={JSON.parse(localStorage.getItem('Profile')).name}
+              />
+            </Grid>
+            <Grid item xs={8} sm={3} md={2} lg={2}>
+              <Button onClick={() => logout()} variant="outlined">
+                Logout
+              </Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     )
